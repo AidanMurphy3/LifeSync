0e81b7751a588cb443e70d37a32b8db718d433ea
safalti <stiwar39@my.centennialcollege.ca>
2025-11-12 18:58:35 -0500

    Made UI component to notify members (3.d)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
 import DeleteConfirmation from './components/DeleteButton.jsx'
+import NotifyUI from './components/NotifyUI.jsx'
 
 function App() {
     return (
-
+<div className='p-4'>
  <DeleteConfirmation />
+ <br />
+ <NotifyUI />
+</div>
 
-    
     )
 }
 
