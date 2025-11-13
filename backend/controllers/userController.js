0d6ca66307a0633901d49385419eed3b9259f93c
k-ghale <kabinghale888@gmail.com>
2025-11-13 01:48:35 -0500

    added getUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,6 +17,19 @@ const getUsers = async (req, res) => {
     }
 };
 
+//Get a User
+const getUser = async (req, res) => {
+    try
+    {
+        const user = await User.findById(req.params.id);
+        if(!user) return res.status(404).json({message : "User not found"});
+        res.status(200).json(user);
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
 
 const addUser = async (req, res) => {
     const { name, email, passwordHash, roleType, status } = req.body;
