fc5f6933709b0fe916feba78d3655239824e2f44
k-ghale <kabinghale888@gmail.com>
2025-11-13 01:51:26 -0500

    added gert User by Name

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,7 @@ const getUsers = async (req, res) => {
 const getUser = async (req, res) => {
     try
     {
-        const user = await User.findById(req.params.id);
+        const user = await User.findById(req.params.name);
         if(!user) return res.status(404).json({message : "User not found"});
         res.status(200).json(user);
     }
@@ -86,4 +86,4 @@ const deleteUser = async (req, res) => {
     }
 };
 
-module.exports = {deleteUser, addUser, getUsers};
+module.exports = {deleteUser, addUser, getUsers, getUsers};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 
 const express = require("express");
 const router = express.Router();
-const {getUsers, deleteUser, addUser} = require("../controllers/userController.js");
+const {getUsers,getUsers, deleteUser, addUser} = require("../controllers/userController.js");
 
 //GET all user route
 router.get("/", getUsers);
+
+//GET user by name 
+router.get("/:name", getUsers);
 
 //ADD user route 
 router.post("/", addUser);
